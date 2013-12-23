a6a5c56339795ceba31499022070659035b57a02
Ed Paradis <legomaniac@gmail.com>
2013-12-22 18:46:36 -0800

    secondary menu for player target selection

--- Assets/Sequencing.cs
+++ Assets/Sequencing.cs
@@ -22,30 +22,30 @@ public class Sequencing : MonoBehaviour {
 	//  I think in the future, instead of AGI determining order, it determines the rate of repeats (DnD style vs later FF style, basically)
 	IEnumerator DoRound()
 	{
-		List<Character> charList = cm.GetChars();
+		List<Character> charList = cm.GetAllChars();
 		charList.Sort( delegate( Character x, Character y)
 		              {  return y.stats.AGI.CompareTo( x.stats.AGI); } );		// reverse sort by AGI (DnD style)
-		foreach( Character c in charList)
+		foreach( Character ch in charList)
 		{
-			if( c.isPC == false)	// if AI controlled
+			if( ch.isPC == false)	// if AI controlled
 			{
 				// ai_mgr.DoBehavior(c, charList); // or i suppose it could do its own CharacterManager.GetChars()
 
 				string result;
 				if( Random.Range(0,2) == 0)
 				{
-					result = c.PhysicalAttack( charList.Find ( delegate( Character z)
+					result = ch.PhysicalAttack( charList.Find ( delegate( Character z)
 					                                          {	return z.isPC;	} ) );	// do a physical attack on the first PC in the list
-					yield return StartCoroutine(ShowEnemyActionLabel( c, "Attack " + result));
+					yield return StartCoroutine(ShowEnemyActionLabel( ch, "Attack " + result));
 				} else {
-					result = c.CastHeal( charList.Find ( delegate( Character z)
+					result = ch.CastHeal( charList.Find ( delegate( Character z)
 					                                    {	return !z.isPC;	} ) );	// cast 'heal' on the first non-PC in the list
-					yield return StartCoroutine(ShowEnemyActionLabel( c, "Heal " + result));
+					yield return StartCoroutine(ShowEnemyActionLabel( ch, "Heal " + result));
 				}
 				// show a graphic or animation
 				yield return new WaitForSeconds( 1f );
 			} else {
-				yield return StartCoroutine(ShowPlayerBattleMenu( c));
+				yield return StartCoroutine(ShowPlayerBattleMenu( ch));
 				// hide the menu and show a graphic or animation
 			}
 
@@ -76,7 +76,9 @@ public class Sequencing : MonoBehaviour {
 	// present the user with a menu of actions in battle
 	IEnumerator ShowPlayerBattleMenu( Character ch)
 	{
-		int selection = 0;
+		Character targetCharacter;
+		string result;
+		int actionSelection = 0;
 		int menuSize = 4;
 		string[] menu = { "Attack", "Defend", "M.Attack", "M.Heal" };
 		// loop grabbing UP and DOWN keys until RETURN is pressed
@@ -86,7 +88,7 @@ public class Sequencing : MonoBehaviour {
 			string menuText = "--" + ch.name + "--";
 			for(int i=0; i<menuSize; i+=1)
 			{
-				if(selection == i)
+				if(actionSelection == i)
 					menuText += "\n> ";
 				else 
 					menuText += "\n   ";
@@ -96,15 +98,100 @@ public class Sequencing : MonoBehaviour {
 
 			// move arrow if UP/DOWN key pressed
 			if( Input.GetKeyDown(KeyCode.UpArrow) )
-				selection = (selection - 1) % menuSize;
+				actionSelection = (actionSelection - 1) % menuSize;
 			if( Input.GetKeyDown(KeyCode.DownArrow) )
-				selection = (selection + 1) % menuSize;
+				actionSelection = (actionSelection + 1) % menuSize;
 			yield return null;
 		}
+
+		// wait for a single frame so that we get another Input event (so we don't immediately select in the next section)
+		yield return new WaitForEndOfFrame();
+
+		// some actions require selecting an enemy or friendly target
+		if(actionSelection == 0 ||	// attack
+		   actionSelection == 2 )	// magic attack
+		{
+			// make a list of the enemies
+			List<Character> enemiesChars = cm.GetNPCs();
+			string[] names = new string[enemiesChars.Count];
+			for(int i=0; i<enemiesChars.Count; i+=1)
+				names[i] = enemiesChars[i].name;
+			yield return StartCoroutine( GenericSelectionMenu( "Choose target", names));
+			targetCharacter = enemiesChars[genericMenuSelection];
+			// now do the thing
+			if( actionSelection == 0)
+			{
+				result = ch.PhysicalAttack( targetCharacter);
+				yield return StartCoroutine(ShowPlayerActionLabel( ch, "Attack " + result));
+				yield return new WaitForSeconds( 1f ); // show a graphic or animation
+			} else {
+				//ch.MagicAttack( targetCharacter);	// though i guess you'll have to select a spell to attack with
+				result = "...thbbbt";
+				yield return StartCoroutine(ShowPlayerActionLabel( ch, "Magic attack " + result));
+				yield return new WaitForSeconds( 1f ); // show a graphic or animation
+			}
+		} else if( actionSelection == 3)	// heal
+		{
+			// make a list of friendlies to heal (including yourself)
+			List<Character> playerChars = cm.GetPCs();
+			string[] names = new string[playerChars.Count];
+			for(int i=0; i<playerChars.Count; i+=1)
+				names[i] = playerChars[i].name;
+			yield return StartCoroutine( GenericSelectionMenu( "Choose target", names));
+			targetCharacter = playerChars[genericMenuSelection];
+
+			result = ch.CastHeal( targetCharacter);
+			yield return StartCoroutine(ShowPlayerActionLabel( ch, "Heal " + result));
+			yield return new WaitForSeconds( 1f ); // show a graphic or animation
+		} else if( actionSelection == 1)	// defend
+		{
+			//pc.SetDefending(true); // or something like hat
+			result = "...thbbbt";
+			yield return StartCoroutine(ShowPlayerActionLabel( ch, "Defend " + result));
+			yield return new WaitForSeconds( 1f ); // show a graphic or animation
+		}
+
+
 		yield return null;
 	}
 
+	int genericMenuSelection;
+	IEnumerator GenericSelectionMenu( string title, string[] options)
+	{
+		genericMenuSelection = 0;
+		int menuSize = options.Length;
+		while( !Input.GetKeyDown(KeyCode.Return) ) 
+		{
+			// draw the menu with a selector arrow on the current option
+			string menuText = title.Clone () as string;
+			for(int i=0; i<menuSize; i+=1)
+			{
+				if(genericMenuSelection == i)
+					menuText += "\n> ";
+				else 
+					menuText += "\n   ";
+				menuText += options[i];
+			}
+			guiText.text = menuText;
+			
+			// move arrow if UP/DOWN key pressed
+			if( Input.GetKeyDown(KeyCode.UpArrow) )
+				genericMenuSelection = (genericMenuSelection - 1) % menuSize;
+			if( Input.GetKeyDown(KeyCode.DownArrow) )
+				genericMenuSelection = (genericMenuSelection + 1) % menuSize;
+			yield return null;
+		}
+		Debug.Log("Generic menu selection = " + genericMenuSelection);
+	}
+
 	IEnumerator ShowEnemyActionLabel( Character ch, string action)
+	{
+		string text = "--" + ch.name + "--\n  " + action;
+		guiText.text = text;
+		yield return null;
+	}
+
+	IEnumerator ShowPlayerActionLabel( Character ch, string action)
 	{
 		string text = "--" + ch.name + "--\n  " + action;
 		guiText.text = text;
