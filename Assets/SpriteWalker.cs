06010ee1c44a69a7b28a0084b659ea250764f125
Ed Paradis <legomaniac@gmail.com>
2013-12-28 17:09:04 -0800

    create a simple walk animation; small script to manually do movement, which I think is wrong

---
+++ Assets/SpriteWalker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteWalker : MonoBehaviour {
+
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.localScale = new Vector3(1,1,1);
+		}
+		if( Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate( -walkSpeed * Time.deltaTime, 0, 0);
+		}
+
+		if( Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.localScale = new Vector3(-1,1,1);
+		}
+		if( Input.GetKey( KeyCode.RightArrow))
+		{
+			transform.Translate( walkSpeed * Time.deltaTime, 0, 0);
+		}
+
+
+	}
+}
