e413a9e0914a48d3b2acf358229d3c3193784e9a
Ed Paradis <legomaniac@gmail.com>
2013-12-30 14:12:37 -0800

    movement works again; no idle/stop animation state

--- Assets/SpriteWalker.cs
+++ Assets/SpriteWalker.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SpriteWalker : MonoBehaviour {
 
+	public float walkSpeed;
 	private Animator animator;
 	
 	// Use this for initialization
@@ -21,20 +22,24 @@ public class SpriteWalker : MonoBehaviour {
 		if (vertical > 0)	// north
 		{
 			animator.SetInteger("Direction", 0);
+			transform.localPosition += new Vector3( 0, walkSpeed * Time.deltaTime, 0);
 		}
 		else if (vertical < 0)	// south
 		{
 			animator.SetInteger("Direction", 2);
+			transform.localPosition += new Vector3( 0, -walkSpeed * Time.deltaTime, 0);
 		}
 		else if (horizontal < 0)	// west
 		{
 			animator.SetInteger("Direction", 3);
 			transform.localScale = new Vector3( 1, 1, 1);	// unflip graphic
+			transform.localPosition += new Vector3( -walkSpeed * Time.deltaTime, 0, 0);
 		}
 		else if (horizontal > 0)	// east
 		{
 			animator.SetInteger("Direction", 3);	// this would be 1, but we dont have flipped graphics, and thus must do so here
 			transform.localScale = new Vector3(-1, 1, 1);	// flip graphic
+			transform.localPosition += new Vector3( walkSpeed * Time.deltaTime, 0, 0);
 		}
 	}
 }
