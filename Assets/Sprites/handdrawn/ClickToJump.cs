2dbff0a63202821072ff3a501f8ef897cbb0132b
Ed Paradis <legomaniac@gmail.com>
2014-02-22 10:24:15 -0800

    figure out how animation works for action games

---
+++ Assets/Sprites/handdrawn/ClickToJump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToJump : MonoBehaviour {
+
+	Animator an;
+
+	// Use this for initialization
+	void Start () {
+		an = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			an.SetTrigger("doJump");
+		}
+	
+		//if(an.GetCurrentAnimatorStateInfo(0).IsName("idle")
+	}
+}
---
+++ Assets/Sprites/handdrawn/KeyboardControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardControl : MonoBehaviour {
+
+	public GameObject character;
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D))
+			transform.Translate( new Vector3( walkSpeed * Time.deltaTime, 0, 0));
+		if(Input.GetKey(KeyCode.A))
+			transform.Translate( new Vector3( -walkSpeed * Time.deltaTime, 0, 0));
+		if(Input.GetKey(KeyCode.W))
+			transform.Translate( new Vector3( 0, walkSpeed * Time.deltaTime, 0));
+		if(Input.GetKey(KeyCode.S))
+			transform.Translate( new Vector3( 0, -walkSpeed * Time.deltaTime, 0));
+		if(Input.GetKeyDown(KeyCode.Space) && !character.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Base Layer.all frames") )
+		{
+			character.GetComponent<Animator>().SetTrigger("doJump");
+		}
+
+	}
+}
