d28136483a084ffd77d2638730457d6f89d5bd10
Ed Paradis <legomaniac@gmail.com>
2014-03-17 14:43:17 -0700

    fill in the other enemy architypes

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -15,6 +15,71 @@ public class HealerArchitype : EnemyBehavior {
 	}
 }
 
+public class MageArchitype : EnemyBehavior {
+
+	public override IEnumerator TakeTurn( List<Character> allChars)
+	{
+		yield return self.helper.StartCoroutine( MagAttackStrongestPC( allChars) );
+	}
+
+	public MageArchitype( Character ch) : base( ch)
+	{
+	}
+}
+
+public class BrawlerArchitype : EnemyBehavior {
+
+	public override IEnumerator TakeTurn( List<Character> allChars)
+	{
+		yield return self.helper.StartCoroutine( PhysAttackStrongestPC( allChars) );
+	}
+
+	public BrawlerArchitype( Character ch) : base( ch)
+	{
+	}
+}
+
+public class MinionArchitype : EnemyBehavior {
+
+	public override IEnumerator TakeTurn( List<Character> allChars)
+	{
+		if( boss.stats.HP < boss.stats.maxHP)	// heals boss (only if its low on health)
+			yield return self.helper.StartCoroutine( Heal( boss) );
+		else if( (float)self.stats.HP < (float)self.stats.HP / 10f)
+			yield return self.helper.StartCoroutine( Defend() );	// defends (if near death)
+		else // attacks strongest player
+			yield return self.helper.StartCoroutine( MagAttackStrongestPC( allChars) );
+	}
+
+	private Character boss;
+
+	public MinionArchitype( Character self, Character boss ) : base( self)
+	{
+		this.boss = boss;
+	}
+}
+
+public class BossArchitype : EnemyBehavior {
+
+	public override IEnumerator TakeTurn( List<Character> allChars)
+	{
+		Character target;
+
+		if( Random.value < 0.5f)
+		{
+			target = FindWeakestPC( allChars);
+			yield return self.helper.StartCoroutine( MagicalAttack(target) );
+		} else {
+			target = FindStrongestPC( allChars);
+			yield return self.helper.StartCoroutine( PhysicalAttack( target) );
+		}
+	}
+
+	public BossArchitype( Character self) : base( self)
+	{
+	}
+}
+
 // if you just want to make a quick behavior that isn't named, or is data driven, use this.
 public class SimpleArchitype : EnemyBehavior {
 
@@ -26,7 +91,7 @@ public class SimpleArchitype : EnemyBehavior {
 		{
 		default:
 		case ActionType.Defend:
-			yield return self.helper.StartCoroutine(Defend( allChars));
+			yield return self.helper.StartCoroutine(Defend());
 			break;
 		case ActionType.Heal:
 			yield return self.helper.StartCoroutine(HealWeakestNPC( allChars));
@@ -64,7 +129,7 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	};
 
 	public enum Architype {
-		Healer, Mage, Brawler, Minion, Boss, Player
+		Healer, Mage, Brawler, Minion, Boss
 	};
 
 	protected Character self;
@@ -75,30 +140,15 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	}
 
 	abstract public IEnumerator TakeTurn( List<Character> allChars);
-	/*public IEnumerator TakeTurn( List<Character> allChars)
-	{
-		switch( QuadRandomPicker( 0.25f, 0.25f, 0.25f, 0.25f) )	// TODO vary these probabilites based on the enemie's architype upon setup
-		{
-		default:
-		case ActionType.Defend:
-			yield return self.helper.StartCoroutine(Defend( allChars));
-			break;
-		case ActionType.Heal:
-			yield return self.helper.StartCoroutine(HealWeakestNPC( allChars));
-			break;
-		case ActionType.MagAttack:
-			yield return self.helper.StartCoroutine(MagAttackStrongestPC( allChars));
-			break;
-		case ActionType.PhysAttack:
-			yield return self.helper.StartCoroutine(PhysAttackStrongestPC( allChars));
-			break;
-		}
-	}*/
 
 	protected IEnumerator PhysAttackStrongestPC( List<Character> allChars )
 	{
 		Character target = FindStrongestPC( allChars);
+		yield return self.helper.StartCoroutine( PhysicalAttack( target) );
+	}
 
+	protected IEnumerator PhysicalAttack( Character target)
+	{
 		if( target != null) // if there are no living characters, just skip this NPC's turn
 		{
 			string resultString = self.PhysicalAttack( target );	// do a physical attack on the first PC in the list
@@ -122,6 +172,11 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	protected IEnumerator HealWeakestNPC( List<Character> allChars)
 	{
 		Character target = FindWeakestNPC( allChars);
+		yield return self.helper.StartCoroutine( Heal( target));
+	}
+
+	protected IEnumerator Heal( Character target)
+	{
 		string resultString = self.CastHeal( target);	// cast 'heal' on the first non-PC in the list
 		self.sfx.PlayOneShot(self.healSfx);
 		yield return self.helper.StartCoroutine(self.notifier.ShowActionLabel( "Heal " + resultString));
@@ -134,6 +189,11 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	protected IEnumerator MagAttackStrongestPC( List<Character> allChars )
 	{
 		Character target = FindStrongestPC( allChars);
+		yield return self.helper.StartCoroutine( MagicalAttack( target) );
+	}
+
+	protected IEnumerator MagicalAttack( Character target )
+	{
 		string resultString = self.CastAttack( target);	// cast 'heal' on the first non-PC in the list
 		self.sfx.PlayOneShot(self.magAttackSfx);
 		yield return self.helper.StartCoroutine(self.notifier.ShowActionLabel( "Magic Attack " + resultString));
@@ -146,7 +206,7 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 		yield return self.helper.StartCoroutine(target.CheckForDeath());
 	}
 
-	protected IEnumerator Defend( List<Character> allChars )	// this parameter is not used but we pass it... um... i don't know why
+	protected IEnumerator Defend( )
 	{
 		string resultString = self.Defend();
 		self.sfx.PlayOneShot( self.defendSfx);
@@ -157,7 +217,7 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	/* ********** */
 
 	// return the NPC which has the least amount of HP
-	Character FindWeakestNPC( List<Character> charList)
+	protected Character FindWeakestNPC( List<Character> charList)
 	{
 		Character target = charList.Find ( delegate( Character z)
 		                                  {     return !z.isPC && !z.isDead;    } ) ;	// TODO this Find is wrong; it just finds the first. not the weakest
@@ -167,12 +227,22 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	}
 
 	// return the PC which has the most amount of HP
-	Character FindStrongestPC( List<Character> charList)
+	protected Character FindStrongestPC( List<Character> charList)
 	{
 		Character target = charList.Find ( delegate( Character z)
 		                                  {     return z.isPC && !z.isDead;    } ) ;	// TODO this Find is wrong; it just finds the first. not the strongest
 		if( target == null)
 			Debug.LogWarning( "FindStrongestPC couldn't find a target");
+		return target;
+	}
+
+	// return the PC which has the least amount of HP
+	protected Character FindWeakestPC( List<Character> charList)
+	{
+		Character target = charList.Find ( delegate( Character z)
+		                                  {     return z.isPC && !z.isDead;    } ) ;	// TODO this Find is wrong; it just finds the first. not the weakest
+		if( target == null)
+			Debug.LogWarning( "FindWeakestPC couldn't find a target");
 		return target;
 	}
 
@@ -187,48 +257,7 @@ public abstract class EnemyBehavior { //: MonoBehaviour {
 	{
 		return null; // TODO
 	}
-
-	/*void HealerArchitype( out ActionType action, out Character target )
-	{
-		// never attacks
-		// often heals weak allies
-		// occasionally defends
-		action = QuadRandomPicker( 0, .25f, 0, .75f);
-		if( action == ActionType.Heal)
-			target = FindWeakestNPC();
-		else
-			target = null;	// defending has no target
-	}
-
-	void MageArchitype( out ActionType action, out Character target )
-	{
-		// always magic attacks
-		// never heals, phys attacks, or defends
-		//QuadRandomPicker(0, 0, 1, 0); 
-		action = ActionType.MagAttack;
-		target = RandomPC ();
-	}*/
-
-	void BrawlerArchitype()
-	{
-		// never heals or magic attacks
-		// defends if low on health
-		// otherwise phys attack
-	}
-
-	void MinionArchitype()
-	{
-		// occasionally attacks
-		// heals boss
-		// occasionally defends
-	}
-
-	void BossArchitype()
-	{
-		// ignores allies
-		// attacks weakest enemy
-	}
-
+	
 	protected ActionType QuadRandomPicker( float physAttackChance, float defendChance, float magAttackChance, float healChance)
 	{
 		// each argument is 0..1, and all should add up to 1
