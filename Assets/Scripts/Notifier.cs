3c3e0c6a21ed97b6e100021506fa658f5e3140c7
Ed Paradis <legomaniac@gmail.com>
2014-03-18 19:30:19 -0700

    rough implementation of notifiers; actually visible and animated

--- Assets/Scripts/Notifier.cs
+++ Assets/Scripts/Notifier.cs
@@ -7,32 +7,58 @@ using System.Collections;
 
 public class Notifier { // : MonoBehaviour {
 
-	GUIText guiText;
 	Character self;
+	GameObject textGO;
+	TextMesh textMesh;
 
 	public Notifier( Character ch )
 	{
 		self = ch;
-		guiText = self.gfx.GetComponent<GUIText>();
-		if( guiText == null)
-		{
-			guiText = self.gfx.AddComponent<GUIText>();
-			Debug.Log("notifier created on " + ch.name );
-		}
 
-		guiText.color = Color.red; // set fonts and colors or whatever
-		// TODO set position to the character's graphic somehow
+		textGO = new GameObject("notifier");
+		textGO.transform.parent = self.gfx.transform;
+		textGO.transform.localPosition = new Vector3( 0f, 1.5f, -1f);
+
+		textMesh = textGO.AddComponent<TextMesh>();
+		//Font font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+		Font font = GameObject.Find("logic").GetComponent<GUIText>().font;
+		textMesh.font = font;
+		textMesh.renderer.material = font.material;
+		textMesh.fontSize = 16;
+		textMesh.characterSize = 0.3f;
+		textMesh.alignment = TextAlignment.Center;
+		textMesh.anchor = TextAnchor.MiddleCenter;
+		textMesh.text = "";
 	}
 
 
-	public IEnumerator ShowActionLabel( string action)
+	public IEnumerator ShowActionLabel( string action) //, Color color = Color.white )
 	{
-		Debug.Log ("inside show action label");
-		string text = "--" + self.name + "--\n  " + action;
-		guiText.text = text;
-		// TODO animate the text floating up or something
-		// TODO probably need to launch a coroutine to go do the animation and clear the text later
-		// TODO also make sure it stays above the our Character (self)
+		//Debug.Log ("inside show action label");
+		//string text = "--" + self.name + "--\n  " + action;
+		string text = action;
+		textMesh.text = text;
+		//textMesh.color = color;
+
+		// launch this but don't yield on it, so the game doesn't 'block' on it
+		self.helper.StartCoroutine(FloatTextUpwardsAndDisappear());
 		yield return null;
 	}
+
+	IEnumerator FloatTextUpwardsAndDisappear()
+	{
+		float startTime = Time.time;
+		float speed = 1f;
+		while( Time.time < startTime + 0.75f)
+		{
+			// TODO animate the text floating up or something
+			textGO.transform.localPosition = Vector3.MoveTowards( textGO.transform.localPosition, new Vector3(0f, 3f, -1f), Time.deltaTime * speed);
+			// TODO probably need to launch a coroutine to go do the animation and clear the text later
+			yield return null;
+		}
+		textMesh.text = "";	// clear when we're done animating
+		textGO.transform.localPosition = new Vector3( 0f, 1.5f, -1f);
+	}
+
+
 }
